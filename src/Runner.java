06461a7ee32ce08e378f0b7828609d57900db07c
wschurman <wschurman@gmail.com>
2012-04-19 22:05:40 -0400

    Yo

--- src/Runner.java
+++ src/Runner.java
@@ -11,7 +11,12 @@ public class Runner {
 		Parser p = new Parser();
 		
 		HashMap<Integer, Question> questions = p.parseQuestions();
-		HashMap<Integer, DocumentSet> documents = p.parseDocs();
+		
+		try {
+			HashMap<Integer, DocumentSet> documents = p.parseDocs();
+		} catch (Exception e) {
+			System.out.println("Error, probably no train/docs directory, download it from http://cl.ly/2c3q3D3Y0T2K2i0o2B0V");
+		}
 	}
 
 }
